33dd85e891ed33806b34bd4a22c432b6ac5c7edc
Niklas Hellström <102542068+nHellstrom@users.noreply.github.com>
2022-11-29 14:01:51 +0100

    Add about page, add axios package

--- src/Pages/About/AboutPage.jsx
+++ src/Pages/About/AboutPage.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import "./AboutPage.css";
 
 const AboutPage = () => {
-    return (
-        <div className="aboutpage__container">
-            
-        </div>
-    )
-}
+  return (
+    <div className="aboutpage__container">
+      <h2>About us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus commodi
+        eius recusandae aut magnam vel voluptas cum maiores consectetur, atque
+        odio, libero, quod perferendis quisquam iusto rerum doloribus. Magnam,
+        explicabo?
+      </p>
+
+      <h2>About the initiative</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus,
+        magnam. Eligendi labore quam mollitia! Deleniti maiores magni sequi.
+        Architecto voluptatem perspiciatis voluptatibus consectetur dolorem
+        quidem tempora minima esse atque ipsum?
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
